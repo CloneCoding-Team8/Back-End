c7340f07fd7aa9d80877ee7860bf6af4a5c07522
guluming <guluming@naver.com>
2022-06-18 08:54:50 +0900

    JWT토큰을 응답해 주기 위한 DTO 추가

---
+++ src/main/java/com/sparta/cloneproject/responsedto/JwtResponseDto.java
@@ -0,0 +1,10 @@
+package com.sparta.cloneproject.responsedto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class JwtResponseDto {
+    private String accessToken;
+}
