534b43dc81d1c7bda5b811cd9f22a38616e93c3e
guluming <guluming@naver.com>
2022-06-18 08:51:46 +0900

    UserController 작성 완료 / api문서에 작성한대로 작성

--- src/main/java/com/sparta/cloneproject/controller/UserController.java
+++ src/main/java/com/sparta/cloneproject/controller/UserController.java
@@ -1,4 +1,35 @@
 package com.sparta.cloneproject.controller;
 
+import com.sparta.cloneproject.requestdto.UserRequestDto;
+import com.sparta.cloneproject.responsedto.JwtResponseDto;
+import com.sparta.cloneproject.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/user")
 public class UserController {
+    private final UserService userService;
+
+    //회원가입
+    @PostMapping("/signup")
+    public boolean signupUser(@RequestBody UserRequestDto singUpData){
+        return userService.signupUser(singUpData);
+    }
+
+    //로그인
+    @PostMapping("/login")
+    public JwtResponseDto loginUser(@RequestBody UserRequestDto loginData){
+        return userService.loginUser(loginData);
+    }
+
+    //회원가입 아이디 중복 확인
+    @PostMapping("/signup/idcheck")
+    public boolean signupUseridCheck(@RequestBody UserRequestDto signupUseridCheckData){
+        return userService.signupUseridCheck(signupUseridCheckData);
+    }
 }
