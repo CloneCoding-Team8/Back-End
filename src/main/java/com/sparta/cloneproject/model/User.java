f41047c707eba7efe299c4722b36173c70ee0af4
guluming <guluming@naver.com>
2022-06-17 18:11:25 +0900

    User 클래스 작성중

--- src/main/java/com/sparta/cloneproject/model/User.java
+++ src/main/java/com/sparta/cloneproject/model/User.java
@@ -1,4 +1,18 @@
 package com.sparta.cloneproject.model;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@Getter
+@Entity
 public class User {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long id;
+    @Column(nullable = false, unique = true)
+    private String username;
+
 }
