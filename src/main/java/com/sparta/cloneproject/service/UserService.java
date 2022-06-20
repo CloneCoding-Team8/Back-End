858f44eb679d76b2d62b44e7965fc8f0b929d673
guluming <guluming@naver.com>
2022-06-20 20:32:24 +0900

    코드정리

--- src/main/java/com/sparta/cloneproject/service/UserService.java
+++ src/main/java/com/sparta/cloneproject/service/UserService.java
@@ -23,8 +23,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.servlet.http.HttpServletRequest;
-import java.util.Date;
 import java.util.Optional;
 
 @RequiredArgsConstructor
