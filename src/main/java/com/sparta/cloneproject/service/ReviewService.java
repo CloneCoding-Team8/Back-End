b5f9ff3fd92ff2308649f90a177ca507a6e72dc3
guluming <guluming@naver.com>
2022-06-21 15:08:26 +0900

    review전체조회 페이징 적용 / review작성 이미지파일 저장 적용 / 다른 기능들 jwt토큰에서 필요정보 가져올 수 있도록 변경

--- src/main/java/com/sparta/cloneproject/service/ReviewService.java
+++ src/main/java/com/sparta/cloneproject/service/ReviewService.java
@@ -5,43 +5,42 @@ import com.sparta.cloneproject.repository.ReviewRepository;
 import com.sparta.cloneproject.requestdto.ReviewRequestDto;
 import com.sparta.cloneproject.responsedto.ReviewResponseDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.transaction.Transactional;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 
 @RequiredArgsConstructor
 @Service
 public class ReviewService {
-
     private final ReviewRepository reviewRepository;
+    private final AwsS3Service s3Service;
+
 
     // Review 조회
-    public List<ReviewResponseDto> getReview(Long productid) {
-        List<Review> reviews = reviewRepository.findAllByProductidOrderByCreatedAtDesc(productid);
-        List<ReviewResponseDto> listReviews = new ArrayList<>();
-        for (Review review : reviews) {
-            ReviewResponseDto reviewsResponseDto = ReviewResponseDto.builder()
-                    .content(review)
-                    .build();
-            listReviews.add(reviewsResponseDto);
-        }
-        return listReviews;
+    public Page<Review> getReview(Long productid, Pageable pageable) {
+//        Sort.Direction direction = isAsc ? Sort.Direction.ASC : Sort.Direction.DESC;
+//        Sort sort = Sort.by(direction, sortBy);
+//        Pageable pageable = PageRequest.of(page, size, sort);
+        return reviewRepository.findAllByProductidOrderByCreatedAtDesc(productid, pageable);
     }
 
     // Review 작성
     @Transactional
-    public Review createReview(ReviewRequestDto requestDto, Long productid, String nickname, String username) {
-//        String reviewCheck = requestDto.getContent();
-//        if (reviewCheck.contains("script") || reviewCheck.contains("<") || reviewCheck.contains(">")) {
-//            Review review = new Review(requestDto, "xss 감지. 내용을 작성할 수 없습니다.");
-//            reviewRepository.save(review);
-//            return review;
-//        }
-        // 요청받은 DTO 로 DB에 저장할 객체 만들기
-        Review review = new Review(requestDto, productid, nickname, username);
+    public Review createReview(Long productid, MultipartFile itemimg,
+                               ReviewRequestDto requestDto,
+                               String nickname,
+                               String username) {
+        Map<String, String> reviewimg = s3Service.uploadFile(itemimg);
+        Review review = new Review(productid, requestDto, reviewimg, nickname, username);
         reviewRepository.save(review);
         return review;
     }
